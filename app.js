37cebf8fd33fe217711f7c685a32125a263785cb
Musa Muhammad <musaawwaletudaye@gmail.com>
2022-03-09 10:04:54 +0100

    feat:4.1 Blog list,step 1

--- app.js
+++ app.js
@@ -1,1 +1,50 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const { PORT, MONGODB_URI } = require("./utils/config");
 
+// const password = process.argv[2];
+
+// const url = `mongodb+srv://Fullstack-Open:${password}@cluster0.ipk8l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// console.log("connecting to:", url);
+
+const url = MONGODB_URI;
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("server connected"))
+  .catch((err) => console.log(err));
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/blogs", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+
+app.post("/api/blogs", (request, response) => {
+  console.log(request.body);
+  const blog = new Blog(request.body);
+
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port here ${PORT}`);
+});
