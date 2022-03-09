e80b6da111e3b56e9a8a2cf8fdbbb0f0821d77ed
Musa Muhammad <musaawwaletudaye@gmail.com>
2022-03-09 14:18:30 +0100

    feat: helper functions and unit tests, step2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,5 +1,12 @@
 const dummy = (blogs) => 1;
 
+const totalLikes = (blogs) => {
+  return blogs.length == 0
+    ? 0
+    : blogs.reduce((accumulator, blog) => accumulator + blog.likes, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
