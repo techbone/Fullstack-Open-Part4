a036a8b18ad6f487a6b8ca02817b2eff3d7d62f6
Musa Muhammad <musaawwaletudaye@gmail.com>
2022-03-09 15:14:19 +0100

    feat: 4.7*: helper functions and unit tests, step5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,54 @@ const totalLikes = (blogs) => {
     : blogs.reduce((accumulator, blog) => accumulator + blog.likes, 0);
 };
 
+const mostBlogs = (blogs) => {
+  const authors = {};
+  let maxBlog = [];
+  blogs.forEach((blog) => {
+    if (!authors[blog.author]) {
+      authors[blog.author] = 1;
+    } else {
+      authors[blog.author] += 1;
+    }
+    if (!maxBlog.length) {
+      maxBlog = [authors[blog.author], blog.author];
+    } else {
+      if (authors[blog.author] > maxBlog[0]) {
+        maxBlog = [authors[blog.author], blog.author];
+      }
+    }
+  });
+
+  const [blog, author] = maxBlog;
+
+  return { author, blog };
+};
+
+const mostLikes = (blogs) => {
+  const authors = {};
+  let likes = [];
+
+  blogs.forEach((blog) => {
+    if (!authors[blog.author]) {
+      authors[blog.author] = blog.likes;
+    } else {
+      authors[blog.author] += blog.likes;
+    }
+    if (!likes.length) {
+      likes = [authors[blog.author], blog.author];
+    } else {
+      if (authors[blog.author] > likes[0]) {
+        likes = [authors[blog.author], blog.author];
+      }
+    }
+  });
+  const [like, author] = likes;
+  return { likes: like, author };
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  mostBlogs,
+  mostLikes,
 };
