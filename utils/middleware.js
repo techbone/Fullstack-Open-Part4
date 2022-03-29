85eb6e3e7b3cbb8760cac052cb3c94aa32e10c14
Musa Muhammad <musaawwaletudaye@gmail.com>
2022-03-29 23:20:35 +0100

    feat:improving

--- utils/middleware.js
+++ utils/middleware.js
@@ -24,6 +24,17 @@ const errorHandler = (error, request, response, next) => {
   next(error);
 };
 
+const errorHandler = (error, request, response, next) => {
+  logger.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
 module.exports = {
   requestLogger,
   unknownEndpoint,
