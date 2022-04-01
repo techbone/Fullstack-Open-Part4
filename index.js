8cf12decd86f98f4c3f87121a1ea00f2c5f89e0d
Musa Muhammad <musaawwaletudaye@gmail.com>
2022-04-01 23:52:55 +0100

    feat:improving

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const logger = require("./utils/logger");
 
 const server = http.createServer(app);
 
-// server.listen(config.PORT, () => {
-//   logger.info(`Server running on portttt ${config.PORT}`);
-// });
+server.listen(config.PORT, () => {
+  logger.info(`Server running on portttt ${config.PORT}`);
+});
